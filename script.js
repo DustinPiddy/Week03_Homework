f896f5a46862a392647477d9efa4845bcf8b4e98
Dustin Pidcock <dustygen@gmail.com>
2020-10-04 19:28:05 -0700

    links the the js to the html and added some user prompts to the js

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var standChar = "abcdefghijklmnopqrstuvwxyz1234567890"
+
+// user choice promt
+var charLength = prompt("Choose between 6 to 10")
+var upperCasesChoice = prompt("Would you like upper cases? 'true' for yes 'false' for no")
+var specialCharactersChoice = prompt("Would you like special cases? 'true' for yes 'false' for no")
 
 // Write password to the #password input
 function writePassword() {
